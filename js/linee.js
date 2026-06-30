66284dcac3d46bf8e6b420dace8cca91ef3051fa
LANCIAY <137568978+LANCIAY@users.noreply.github.com>
2026-06-30 14:43:23 +0200

    Update linee.js

--- js/linee.js
+++ js/linee.js
@@ -1,1 +1,21 @@
+fetch("data/linee.json")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("lineeContainer");
 
+    data.forEach(linea => {
+      const div = document.createElement("div");
+
+      div.className = "card";
+      div.innerHTML = `
+        <h3>${linea.nome}</h3>
+        <p>${linea.capolinea}</p>
+      `;
+
+      div.onclick = () => {
+        window.location.href = `linea.html?id=${linea.id}`;
+      };
+
+      container.appendChild(div);
+    });
+  });
