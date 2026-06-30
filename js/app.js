943db3d5ffc405229a3e8c82467917fd7b30f950
LANCIAY <137568978+LANCIAY@users.noreply.github.com>
2026-06-30 14:35:19 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+// Mappa base centrata su Bagheria
+const map = L.map('map').setView([38.079, 13.510], 13);
 
+// OpenStreetMap layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+// Marker esempio (fermata finta)
+L.marker([38.079, 13.510])
+  .addTo(map)
+  .bindPopup("Centro Bagheria")
+  .openPopup();
